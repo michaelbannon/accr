60acb9710f4fb21758a8f4cb585fa9aa668ff1b1
Michael Bannon <michaelbannon@michaels-mbp.home>
2023-11-09 15:24:01 +0000

    feat: initial bet algorithm added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
+import data from './data/mock-data'
 import './App.css';
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route path="betform" element={<BetForm />} />
+      </Route>
       <Route path="*" element={<NoMatch />} />
     </Routes>
   )
@@ -21,14 +24,86 @@ const Home = () => {
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      <Outlet/>
+    </>
   )
 };
+
+const sortByHighestOdds = (data) => {
+  const sortedOdds = [];
+  data.forEach((game) => {
+    const sortedGame = {...game};
+    sortedGame.bookmakers[0].markets[0].outcomes.sort((a, b) => b.price - a.price);
+    sortedOdds.push(sortedGame);
+  })
+  return sortedOdds
+}
+
+const createBet = (betFormObj, sortedOdds) => {
+  const { stake, risk, gameAmount } = betFormObj;
+  const riskEval = [2, 1, 0];
+  const bet = sortedOdds.map((betOdds) => {
+    const selectedBet = betOdds.bookmakers[0].markets[0].outcomes[riskEval[risk]];
+    return {
+	    teams: `${betOdds.home_team} v ${betOdds.away_team}`,
+	    bet: selectedBet
+	  }
+  });
+  const betLength = bet.length - 1;
+  let randomStartPoint = bet.length;
+  while ((randomStartPoint + gameAmount) > betLength) {
+    randomStartPoint = Math.floor(Math.random() * betLength);
+  }
+  return bet.slice(randomStartPoint, randomStartPoint + gameAmount);
+}
+
+const BetForm = () => {
+  const [betForm, setBetForm] = useState({});
+  const [sortedOdds, setSortedOdds] = useState([]);
+
+  useEffect(() => {
+    setSortedOdds(sortByHighestOdds(data))
+  },[])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBetForm((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const testObj = {
+      stake: 10,
+      risk: 2,
+      gameAmount: 4
+    }
+  }
+
+  return (
+    <form className="bet-form" onSubmit={handleSubmit}>
+      <div className="bet-form__stake">
+        <label htmlFor="stake">Stake</label>
+        <input name="stake" type="number" min="0.00" max="10000.00" step="0.01" />
+      </div>
+      <div className="bet-form__risk">
+        <label htmlFor="risk">Risk</label>
+        
+      </div>
+      <button>Submit</button>
+    </form>
+    
+  )
+}
 
 const NoMatch = () => {
   return (
     <h1>404</h1>
   )
-}
+};
 
 export default App;
---
+++ client/src/data/mock-data.js
@@ -0,0 +1,704 @@
+const data = [
+  {
+    "id": "8bedee5c03bbc287c1a4058b924110da",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-11T12:30:00Z",
+    "home_team": "Wolverhampton Wanderers",
+    "away_team": "Tottenham Hotspur",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Tottenham Hotspur",
+                "price": 2.1
+              },
+              {
+                "name": "Wolverhampton Wanderers",
+                "price": 3.1
+              },
+              {
+                "name": "Draw",
+                "price": 3.6
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "cc60420ead345d747ccb88dd51ceed31",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-11T15:00:00Z",
+    "home_team": "Arsenal",
+    "away_team": "Burnley",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Arsenal",
+                "price": 1.17
+              },
+              {
+                "name": "Burnley",
+                "price": 16
+              },
+              {
+                "name": "Draw",
+                "price": 7
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "32f31abcdd421b752aa94deccd5ace3d",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-11T15:00:00Z",
+    "home_team": "Crystal Palace",
+    "away_team": "Everton",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Crystal Palace",
+                "price": 2.4
+              },
+              {
+                "name": "Everton",
+                "price": 3
+              },
+              {
+                "name": "Draw",
+                "price": 3.1
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "284cfe2b6e96b7b6fd7181d23fdd497f",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-11T15:00:00Z",
+    "home_team": "Manchester United",
+    "away_team": "Luton",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Luton",
+                "price": 9
+              },
+              {
+                "name": "Manchester United",
+                "price": 1.29
+              },
+              {
+                "name": "Draw",
+                "price": 5.5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "b6b84857cc01b13df6ecb3fefdbe77fc",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-11T17:30:00Z",
+    "home_team": "Bournemouth",
+    "away_team": "Newcastle United",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Bournemouth",
+                "price": 4.75
+              },
+              {
+                "name": "Newcastle United",
+                "price": 1.67
+              },
+              {
+                "name": "Draw",
+                "price": 3.8
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "71556be3250ba24694bf32d8fb01762c",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-12T14:00:00Z",
+    "home_team": "Aston Villa",
+    "away_team": "Fulham",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Aston Villa",
+                "price": 1.57
+              },
+              {
+                "name": "Fulham",
+                "price": 5.5
+              },
+              {
+                "name": "Draw",
+                "price": 4
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "4ea1aee43fd1e3905feae33d21030523",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-12T14:00:00Z",
+    "home_team": "Liverpool",
+    "away_team": "Brentford",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Brentford",
+                "price": 7.5
+              },
+              {
+                "name": "Liverpool",
+                "price": 1.33
+              },
+              {
+                "name": "Draw",
+                "price": 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "57039c4c1858482a8c81ed847914ef73",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-12T14:00:00Z",
+    "home_team": "Brighton and Hove Albion",
+    "away_team": "Sheffield United",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Brighton and Hove Albion",
+                "price": 1.2
+              },
+              {
+                "name": "Sheffield United",
+                "price": 11
+              },
+              {
+                "name": "Draw",
+                "price": 6.5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "39fb3c39238d0954ca4e7e9a920b24b6",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-12T14:00:00Z",
+    "home_team": "West Ham United",
+    "away_team": "Nottingham Forest",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Nottingham Forest",
+                "price": 4.2
+              },
+              {
+                "name": "West Ham United",
+                "price": 1.8
+              },
+              {
+                "name": "Draw",
+                "price": 3.75
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "fd269f7ec3b470512d0e8285756113ff",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-12T16:30:00Z",
+    "home_team": "Chelsea",
+    "away_team": "Manchester City",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Chelsea",
+                "price": 4.5
+              },
+              {
+                "name": "Manchester City",
+                "price": 1.73
+              },
+              {
+                "name": "Draw",
+                "price": 3.8
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "4acd8f2675ca847ba33eea3664f6c0bb",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T12:30:00Z",
+    "home_team": "Manchester City",
+    "away_team": "Liverpool",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Liverpool",
+                "price": 4.5
+              },
+              {
+                "name": "Manchester City",
+                "price": 1.62
+              },
+              {
+                "name": "Draw",
+                "price": 4.33
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "90bf99259813b52c75ee2aba5d42b654",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T15:00:00Z",
+    "home_team": "Sheffield United",
+    "away_team": "Bournemouth",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Bournemouth",
+                "price": 2.25
+              },
+              {
+                "name": "Sheffield United",
+                "price": 3
+              },
+              {
+                "name": "Draw",
+                "price": 3.4
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "092c34b4e1078f45256cddc569532981",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T15:00:00Z",
+    "home_team": "Nottingham Forest",
+    "away_team": "Brighton and Hove Albion",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Brighton and Hove Albion",
+                "price": 2
+              },
+              {
+                "name": "Nottingham Forest",
+                "price": 3.4
+              },
+              {
+                "name": "Draw",
+                "price": 3.75
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "e5eab69b037b798f8cd688d48445f5a6",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T15:00:00Z",
+    "home_team": "Burnley",
+    "away_team": "West Ham United",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Burnley",
+                "price": 3.2
+              },
+              {
+                "name": "West Ham United",
+                "price": 2.15
+              },
+              {
+                "name": "Draw",
+                "price": 3.4
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "8d7f2b52124699c72c5e57ec59b37f41",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T15:00:00Z",
+    "home_team": "Newcastle United",
+    "away_team": "Chelsea",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Chelsea",
+                "price": 3.4
+              },
+              {
+                "name": "Newcastle United",
+                "price": 2.1
+              },
+              {
+                "name": "Draw",
+                "price": 3.5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "f79c939c5f96eb2dd8efcb52f11ccb88",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T15:00:00Z",
+    "home_team": "Luton",
+    "away_team": "Crystal Palace",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Crystal Palace",
+                "price": 2
+              },
+              {
+                "name": "Luton",
+                "price": 3.75
+              },
+              {
+                "name": "Draw",
+                "price": 3.3
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1766194919f1cbfbd846576434f0499b",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-25T17:30:00Z",
+    "home_team": "Brentford",
+    "away_team": "Arsenal",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Arsenal",
+                "price": 1.8
+              },
+              {
+                "name": "Brentford",
+                "price": 4
+              },
+              {
+                "name": "Draw",
+                "price": 3.75
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1274e00f4f8e15ca717d17e78a12d73e",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-26T14:00:00Z",
+    "home_team": "Tottenham Hotspur",
+    "away_team": "Aston Villa",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Aston Villa",
+                "price": 3
+              },
+              {
+                "name": "Tottenham Hotspur",
+                "price": 2.1
+              },
+              {
+                "name": "Draw",
+                "price": 3.8
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "25d807513a34bdf91a61b363ef0a985f",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-26T16:30:00Z",
+    "home_team": "Everton",
+    "away_team": "Manchester United",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Everton",
+                "price": 3
+              },
+              {
+                "name": "Manchester United",
+                "price": 2.3
+              },
+              {
+                "name": "Draw",
+                "price": 3.4
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "1a0f89babacc99aebc55f4c83428c472",
+    "sport_key": "soccer_epl",
+    "sport_title": "EPL",
+    "commence_time": "2023-11-27T20:00:00Z",
+    "home_team": "Fulham",
+    "away_team": "Wolverhampton Wanderers",
+    "bookmakers": [
+      {
+        "key": "paddypower",
+        "title": "Paddy Power",
+        "last_update": "2023-11-09T11:44:19Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2023-11-09T11:44:19Z",
+            "outcomes": [
+              {
+                "name": "Fulham",
+                "price": 2.15
+              },
+              {
+                "name": "Wolverhampton Wanderers",
+                "price": 3.4
+              },
+              {
+                "name": "Draw",
+                "price": 3.3
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default data;
